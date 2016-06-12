8c5535e6a5b209de32deca3e32cfcfab99f4f63b
Shin <mrmaxin@gmail.com>
2016-06-11 21:03:59 -0700

    refactored article_params method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,9 +4,14 @@ class ArticlesController < ApplicationController
 
   def create
     #render plain: params[:article].inspect
-    @article = Article.new(params.require(:article).permit(:title, :text))
+    @article = Article.new(article_params)
 
     @article.save
     redirect_to @article
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 end
