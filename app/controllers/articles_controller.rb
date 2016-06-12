3cf1f4dd92fdb399b2bd965cd0024f81ef21edec
Shin <mrmaxin@gmail.com>
2016-06-11 21:02:41 -0700

    saving data in the controller with strong params

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,7 @@ class ArticlesController < ApplicationController
 
   def create
     #render plain: params[:article].inspect
-    @article = Article.new(params[:article])
+    @article = Article.new(params.require(:article).permit(:title, :text))
 
     @article.save
     redirect_to @article
